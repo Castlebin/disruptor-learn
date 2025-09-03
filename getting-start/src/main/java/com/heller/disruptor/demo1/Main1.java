a048fafbe099ab39f7b03ff018c1c28ef7ac27b9
Castlebin <Castlebin@gmail.com>
2025-09-03 16:27:44 +0800

    函数式编程

---
+++ getting-start/src/main/java/com/heller/disruptor/demo1/Main1.java
@@ -0,0 +1,58 @@
+package com.heller.disruptor.demo1;
+
+import java.nio.ByteBuffer;
+
+import com.lmax.disruptor.BlockingWaitStrategy;
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+import com.lmax.disruptor.dsl.ProducerType;
+import com.lmax.disruptor.util.DaemonThreadFactory;
+
+/**
+ * 函数式编程
+ */
+public class Main1 {
+    public static void main(String[] args) throws Exception {
+        // Ring Buffer 大小，必须是2的幂
+        int bufferSize = 1024;
+
+        // 1. 创建 Disruptor 实例
+        Disruptor<LongEvent> disruptor = new Disruptor<>(
+                LongEvent::new,
+                bufferSize,
+                DaemonThreadFactory.INSTANCE,
+                // 单生产者模式
+                ProducerType.SINGLE,
+                new BlockingWaitStrategy()
+        );
+
+        // 2. 注册事件处理器
+        disruptor.handleEventsWith(
+                (event, sequence, endOfBatch)
+                        ->
+                        System.out.println("Event: " + event.getValue()));
+
+        // 3. 启动 Disruptor
+        disruptor.start();
+
+        // 4. 获取 Ring Buffer
+        RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
+
+        // 5. 创建生产者
+        LongEventProducer producer = new LongEventProducer(ringBuffer);
+
+        // 6. 模拟生产者发送数据
+        ByteBuffer byteBuffer = ByteBuffer.allocate(8);
+        for (long value = 0; true; value++) {
+            // 生产者生产数据
+            producer.onData(byteBuffer.putLong(0, value));
+
+            // 模拟发送间隔
+            Thread.sleep(1000);
+        }
+
+        // 7. 关闭 Disruptor (如果循环结束的话)
+        //disruptor.shutdown();
+    }
+
+}
